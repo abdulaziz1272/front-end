791f059f3e9fc87e7f3cf17037d67e2cfa07b03d
Abdulaziz <abdurashidovabdulaziz1272@gmail.com>
2025-11-05 14:11:08 +0500

    sefa

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,6 +1,49 @@
 <template>
-    <div>
-        Footer
+    <div class="main-footer">
+        <div class="footer-top">
+            <div>
+                <h3>Топ категории</h3>
+                <ul class="text-ul">
+                    <li>Тексты и переводы</li>
+                    <li>Разработка</li>
+                    <li>Дизайн</li>
+                    <li>Аудио, видео монтаж</li>
+                    <li>Соцсети и реклама</li>
+                    <li>Бизнес и жизнь</li>
+                    <li>SEO и оптимизация</li>
+                </ul>
+            </div>
+            <div>
+                <h3>О Проекте</h3>
+                <ul class="text-ul">
+                    <li>О Нас</li>
+                    <li>Как Это Работает</li>
+                    <li>Политика Приватности</li>
+                    <li>Правила Пользования </li>
+                    <li>Пресса о нас</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Поддержка</h3>
+                <ul class="text-ul">
+                    <li>Контакты</li>
+                    <li>Политика Безопасности</li>
+                    <li>FAQ</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Follow</h3>
+                <ul class="icon-ul">
+                    <li><i class="fa-brands fa-square-facebook"></i></li>
+                    <li><i class="fa-brands fa-square-twitter"></i></li>
+                    <li><i class="fa-brands fa-square-instagram"></i></li>
+                    <li><i class="fa-brands fa-linkedin"></i></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>Copyright @ 2021  |  WorkTap – Worktap.KZ. All Rights Reserved</p>
+        </div>
     </div>
 </template>
 <script>
@@ -8,6 +51,47 @@ export default {
     
 }
 </script>
-<style>
-    
+<style scoped>
+.main-footer {
+    background-color: #F2F0FE;
+    padding: 50px 120px 100px 120px;
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+}
+.footer-top{
+    display: flex;
+    justify-content: space-between;
+    >div{
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+
+        h3{
+            font-size: 24px;
+            font-weight: 700;
+        }
+        .text-ul{
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+
+        }
+        .icon-ul{
+            display: flex;
+            gap: 15px;
+            font-size: 45px;
+            li{
+                transform: translateX(-10px);
+            }
+        }
+    }
+}
+.footer-bottom{
+    text-align: center;
+}
+ul{
+    list-style: none;
+    padding: 0;
+}
 </style>
--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,19 @@
 <template>
-    <div>
-        Header
+    <div class="main-header">
+        <div class="header-left">
+            <img src="../../assets/icons/logo.svg" alt="logo">
+            <ul>
+                <li>Биржа</li>
+                <li>Ворки</li>
+                <li>Конкурсы</li>
+                <li>Создать ворк</li>
+                <li>Создать заказ</li>
+            </ul>
+        </div>
+        <div class="header-right">
+            <button>Регистрация</button>
+            <button>Войти</button>
+        </div>
     </div>
 </template>
 <script>
@@ -8,6 +21,51 @@ export default {
     
 }
 </script>
-<style>
-    
+<style scoped>
+.main-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 30px 120px;
+    background-color: #fff;
+}
+.header-left{
+    display: flex;
+    align-items: center;
+    gap: 70px;
+    ul{
+        display: flex;
+        list-style: none;
+        gap: 40px;
+        li{
+            font-size: 16px;
+            font-weight: 500;
+            color: #333;
+            cursor: pointer;
+        }
+    }
+}
+.header-right{
+    display: flex;
+    gap: 30px;
+    button{
+        height: 40px;
+        border-radius: 20px;
+        border: none;
+        outline: none;
+        font-size: 16px;
+        font-weight: 600;
+        cursor: pointer;
+    };
+    button:first-child{
+        width: 170px;
+        background-color: #F2F0FE;
+        color: #1DBF73;
+    };
+    button:last-child{
+        width: 110px;
+        background-color: #1DBF73;
+        color: #fff;
+    };
+}
 </style>
