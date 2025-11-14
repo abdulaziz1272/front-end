17a5547df8a102644824d8ddcce7092ad79fb3ac
ibrohim3 <ibrohim.2128382@gmail.com>
2025-11-14 20:56:20 +0500

    contest page added

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,8 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
     { path: '/', name: 'Home', component: () => import("../pages/Home/Home.vue") },
     { path: '/profile', name: 'Profile', component: () => import('../pages/Profile/Profile.vue') },
-    { path: '/profile/history', name: 'History', component: () => import('../pages/Profile/History/History.vue') }
+    { path: '/profile/history', name: 'History', component: () => import('../pages/Profile/History/History.vue') },
+    { path: "/contest", name: "Contest", component: () => import("../pages/Contest/Contest.vue") },
 
 ]
 
